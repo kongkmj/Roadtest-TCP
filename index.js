9c2978dadda9a55255aea4614d130cf3717b761a
Minjae-Kim <asd789zx@naver.com>
2016-05-23 19:53:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ server.listen(11111,function () {
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-mongoose.connect("mongodb://test:test@ds017672.mlab.com:17672/graph");
+mongoose.connect("mongodb://#");
 var db = mongoose.connection;
 db.once("open",function () {
   console.log("DB connected");
